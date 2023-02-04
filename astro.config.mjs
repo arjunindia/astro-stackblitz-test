24a6e00f0618c9c253f24f460defc6b438ac4514
arjunindia <37960163+arjunindia@users.noreply.github.com>
2023-02-04 23:59:45 +0530

    5 files modified

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,10 @@ import prefetch from '@astrojs/prefetch';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
+import image from "@astrojs/image";
+
+// https://astro.build/config
 export default defineConfig({
   site: 'https://steady-puffpuff-e28869.netlify.app/',
-  integrations: [preact(), prefetch(), sitemap()],
+  integrations: [preact(), prefetch(), sitemap(), image()]
 });
